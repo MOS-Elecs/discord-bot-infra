2a65db827ec784d70bf3cf300b55033d78165dca
53175DDD <53175ddd@gmail.com>
2026-08-21 19:00:39 +0900

    不具合修正

--- scripts/proxmox/03-bootstrap-flux.sh
+++ scripts/proxmox/03-bootstrap-flux.sh
@@ -3,12 +3,13 @@ set -euo pipefail
 
 curl -s https://fluxcd.io/install.sh | sudo bash
 
-export GITHUB_TOKEN=<PAT>
-export GITHUB_USER=<your-github-user>
+# 実行前に以下を環境変数としてexportしておく
+#   export GITHUB_TOKEN=<GitHubのPersonal Access Token>
+#   export GITHUB_USER=MOS-Elecs
 
 flux bootstrap github \
   --owner=$GITHUB_USER \
   --repository=discord-bot-infra \
   --branch=main \
   --path=clusters/prod \
-  --personal
+  --components-extra=image-reflector-controller,image-automation-controller
